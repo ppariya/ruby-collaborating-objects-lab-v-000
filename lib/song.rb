7267d6e9883c39115be8d63ca2f1cb913a47fb64
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-22 06:19:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist
 
   def self.new_by_filename(file_name)
-    song = self.new(file_name)
+    song = self.new
     song.name = file_name.split(" - ")[1]
 
   end
