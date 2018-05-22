de7d422e200eecfab8b63585cb0eb71b6a750715
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-22 06:22:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,6 @@ class Song
   def self.new_by_filename(file_name)
     artist, song = file_name.split(" - ")
     newsong = self.new(song)
-    newsong.artist.name = artist
+    newsong.artist_name = artist
   end
 end
